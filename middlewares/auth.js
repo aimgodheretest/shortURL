80595dc4b4bfd6e395ded2212cf946ea86e88004
Murli Kumar <aimgodhere@gmail.com>
2026-07-15 11:51:32 +0530

    standardize auth token parsing

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,11 @@
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.cookies?.uid;
+  const userUid = req.headers["authorization"];
 
   if (!userUid) return res.redirect("/login");
-  const user = getUser(userUid);
+  const token = userUid.split("Bearer ")[1];
+  const user = getUser(token);
 
   if (!user) return res.redirect("/login");
 
@@ -13,9 +14,14 @@ async function restrictToLoggedinUserOnly(req, res, next) {
 }
 
 async function checkAuth(req, res, next) {
-  const userUid = req.cookies?.uid;
+  const userUid = req.headers["authorization"];
 
-  const user = getUser(userUid);
+  if (!userUid) {
+    req.user = null;
+    return next();
+  }
+  const token = userUid.split("Bearer ")[1];
+  const user = getUser(token);
 
   req.user = user;
   next();
